e81eabfb1ab4f5c0c3228170fb8f231b841c8ef9
Tim Cooijmans <timcooijmans@gmail.com>
2022-10-31 13:30:56 +0100

    Fix test

--- bitoffset_test.go
+++ bitoffset_test.go
@@ -34,12 +34,6 @@ func TestUint32(t *testing.T) {
 		t.Errorf("got: %d, want %d", got, 2)
 	}
 
-	buf = []byte{0x0e, 0xd1, 0x14, 0x90, 0x1b, 0xef, 0x9c, 0x2f, 0xa1, 0x54, 0x05, 0xb4, 0xed, 0x07, 0x88}
-	got = bitoffset.Uint32(buf, 4, 28)
-	if got != 3 {
-		t.Errorf("got: %d, want %d", got, 3)
-	}
-
 }
 
 func TestInt32(t *testing.T) {
